f041efabafd21c56d8a9e2b5ef32af040ffd46dc
tandv18-art <tandv18@gmail.com>
2026-09-12 14:57:43 +0700

    WEB20: pass explicit OIDC credentials when reading private Blob

--- api/media.js
+++ api/media.js
@@ -10,7 +10,16 @@ function send(res, status, body) {
 }
 
 function blobAuthOptions() {
-  return process.env.BLOB_READ_WRITE_TOKEN ? { token: process.env.BLOB_READ_WRITE_TOKEN } : {};
+  if (process.env.BLOB_READ_WRITE_TOKEN) {
+    return { token: process.env.BLOB_READ_WRITE_TOKEN };
+  }
+  if (process.env.VERCEL_OIDC_TOKEN && process.env.BLOB_STORE_ID) {
+    return {
+      oidcToken: process.env.VERCEL_OIDC_TOKEN,
+      storeId: process.env.BLOB_STORE_ID
+    };
+  }
+  return {};
 }
 
 module.exports = async function handler(req, res) {
