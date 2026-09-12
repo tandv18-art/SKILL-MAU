17e4808aee6c261384b555e7a262d234acbe8f9c
tandv18-art <tandv18@gmail.com>
2026-09-12 15:26:07 +0700

    WEB21: gate virtual try-on by authenticated image usage

--- api/virtual-tryon.js
+++ api/virtual-tryon.js
@@ -1,2 +1,3 @@
 const { generateTryon } = require('../lib/image-storage-runtime');
-module.exports = generateTryon;
+const { withUsageGate } = require('../lib/usage-gated-handler');
+module.exports = withUsageGate(generateTryon, { kind: 'image', skillId: 'virtual-tryon' });
